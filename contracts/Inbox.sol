4e1c8cd8e7b8aeba29e1f0676ee8dc2f379a5b94
Andrew <afuselier23@gmail.com>
2018-08-04 10:48:50 -0700

    Adding comments for smart contract

--- contracts/Inbox.sol
+++ contracts/Inbox.sol
@@ -1,5 +1,10 @@
 pragma solidity ^0.4.24;
 
+/**
+ * Simple contract for testing. Defines one instance variable called 'message' that stores
+ * the contract state. The contructor sets the initial message. And the setMessage method
+ * updates the contract state.
+*/
 contract Inbox {
   string public message;
 
